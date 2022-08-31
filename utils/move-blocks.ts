883b47348cfa62de9f92aa9f733c01e8c6952f8d
Kizito007 <nwakakelechi49@gmail.com>
2022-08-31 11:40:29 +0100

    initial commit

---
+++ utils/move-blocks.ts
@@ -0,0 +1,11 @@
+const { network } = require("hardhat");
+
+export async function moveBlocks(amount: number) {
+  console.log("Moving blocks...");
+  for (let index = 0; index < amount; index++) {
+    await network.provider.request({
+      method: "evm_mine",
+      params: [],
+    });
+  }
+}
